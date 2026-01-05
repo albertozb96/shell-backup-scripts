be438b306a22baaaef2da08f36d5a3c786d003ca
J. Alberto Zapata <albertozb96@gmail.com>
2026-01-05 02:21:32 +0100

    Update cron-script.sh

--- main/cron-script.sh
+++ main/cron-script.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-"${HOME}/shell-backup-scripts/main/daily-snapshot.sh" -v && "${HOME}/shell-backup-scripts/main/purge-snapshots.sh" #>> "${HOME}/Snapshots/purge.log"
+"${HOME}/shell-backup-scripts/main/daily-snapshot.sh" -v && "${HOME}/shell-backup-scripts/main/purge-snapshots.sh" >> "${HOME}/Snapshots/purge.log"
+"${HOME}/shell-backup-scripts/main/daily-snapshot-support.sh" -v && "${HOME}/shell-backup-scripts/main/purge-snapshots-support.sh" >> "${HOME}/Snapshots-Support/purge.log"
